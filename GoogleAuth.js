ece3f9de2a08c527522ec22a9d41d95b5546ff73
samarvir <samarvir1996@gmail.com>
2020-01-10 00:00:40 +0530

    add google signin

---
+++ GoogleAuth.js
@@ -0,0 +1,78 @@
+import * as Google from "expo-google-app-auth";
+import * as firebase from "firebase";
+import { firebaseConfig } from "./config";
+firebase.initializeApp(firebaseConfig);
+
+isUserEqual = (googleUser, firebaseUser) => {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (
+        providerData[i].providerId ===
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+        providerData[i].uid === googleUser.getBasicProfile().getId()
+      ) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+onSignIn = googleUser => {
+  console.log("Google Auth Response", googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+        googleUser.idToken,
+        googleUser.accessToken
+      );
+      // Sign in with credential from the Google user.
+      firebase
+        .auth()
+        .signInWithCredential(credential)
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    } else {
+      console.log("User already signed-in Firebase.");
+    }
+  });
+};
+
+signInWithGoogleAsync = async () => {
+  try {
+    const result = await Google.logInAsync({
+      behavior: "web",
+      androidClientId:
+        "1066704227169-e8qdgf2akv22ivhbglmgihj12hiitbdq.apps.googleusercontent.com",
+      iosClientId:
+        "1066704227169-jgmb9skovr7rl61mt8i824e5llsfse0l.apps.googleusercontent.com",
+      scopes: ["profile", "email"]
+    });
+
+    if (result.type === "success") {
+      console.log("signed in");
+      this.onSignIn(result);
+      return result.accessToken;
+    } else {
+      return { cancelled: true };
+    }
+  } catch (e) {
+    return { error: true };
+  }
+};
+
+export default signInWithGoogleAsync;
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4MhMs5tFxf8P0Cs1SVsVtk6H7g3PeEJI",
+  authDomain: "illuminati-32f56.firebaseapp.com",
+  databaseURL: "https://illuminati-32f56.firebaseio.com",
+  projectId: "illuminati-32f56",
+  storageBucket: "illuminati-32f56.appspot.com",
+  messagingSenderId: "1066704227169",
+  appId: "1:1066704227169:web:8068a8b11786ece0e31320",
+  measurementId: "G-2JCQTCJPYH"
+};
